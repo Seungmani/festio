3dfd648ac1a0cd6634ec6bc82c7b43a22db97856
Seungmani <dkelcncjs@naver.com>
2024-10-01 17:01:57 +0900

    [feat] API 연동 및 Redux-Query 생성

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,11 +1,14 @@
-// src/store/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import { api } from "./api";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 
